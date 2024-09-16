9b9b0b77d88680b610774e6b4eecc87275a296b5
philippeysl20 <rd57yh@student.aau.dk>
2024-09-16 02:40:31 +0200

    fn

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+int number;
+    printf("Enter amount in your bank account: "), &number;
+    scanf("%d",&number);
+
+    if (number <= 300)
+        printf("broke ass fuck nigga");
+    else
+        printf("y'aight whiteboy, gimmie one fuck nigga");
+    return 0;
+}
